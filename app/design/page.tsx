4f0f6ddc025cd6d05314d953d1fb44249e9afbd8
Elia Albaba <ea152@uakron.edu>
2024-10-27 11:38:56 -0400

    Add formatting to designs

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -2,16 +2,25 @@ import Image from "next/image";
 
 export default function Design() {
   return (
-    <div className = "p-4">
+    <div>
         <h1 className = "py-5 text-5xl text-bold justify-center items-center flex"  >Design</h1>
         <h2 className = "py-3 text-2xl justify-center items-center flex" >A collection of designs I've created over the years!</h2>
-        <Image
-        //   className="dark:invert"
-          src="/circuit_design.png"
-          alt="Vercel logomark"
-          width={200}
-          height={200}
-        />
+        <div className = "background-color-orange justify-center flex">
+          {/* row 1 */}
+          <div>
+            <Image
+              className=" w-64 h-64 object-cover object-center"
+              src="/circuit_design.png"
+              alt="Vercel logomark"
+              width={500}
+              height={500}
+            />
+            <p className = "w-64 h-64">
+            oerm ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            </p>
+          </div>
+
+        </div>
 
     </div>
   );
