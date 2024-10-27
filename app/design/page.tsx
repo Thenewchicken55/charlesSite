298f505c19166f57a9d637651ed44a3d832a8e82
Elia Albaba <ea152@uakron.edu>
2024-10-27 11:18:31 -0400

    Add design title

--- app/design/page.tsx
+++ app/design/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Design() {
   return (
-    <div>
+    <div className = "p-4">
+        <h1 className = "py-5 text-5xl text-bold justify-center items-center flex"  >Design</h1>
+        <h2 className = "py-3 text-2xl justify-center items-center flex" >A collection of designs I've created over the years!</h2>
         <Image
         //   className="dark:invert"
           src="/circuit_design.png"
